3e61177c3a2b764aa59b9751a8435939a3763805
Ezequiel Stella <ezequielstella@gmail.com>
2023-03-30 12:21:19 +0200

    Update settings.py

--- distribuidora/settings.py
+++ distribuidora/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'distribuidora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'distribuidora.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'pfEALTGzsIAsiksntIKT',
+        'HOST': 'containers-us-west-98.railway.app',
+        'PORT': '7057',
     }
 }
 
